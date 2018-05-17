4061b17de4f0dedb3acd69025f671dd74b6f1c5f
David Costa <david.costa@codeminer42.com>
2018-05-17 15:19:13 -0300

    Add show and index views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :tasks
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, :tasks
+
+  get '/users/:id/tasks', to: 'users#tasks', as: :user_tasks
 end
